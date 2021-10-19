e422af3c798dc6eba8e2ca3714b07ace009159a6
Eeshita <biswas@udel.edu>
2021-10-19 12:13:37 -0400

    Initial push to Github

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                UD CIS Scheduler
+                <p>
+                 Hello! Welcome to your Degree Plan!
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
